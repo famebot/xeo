49c2a619b7b4a4cb7f52c42001df0a0cad806f8e
Ricky de Laveaga <git@rdela.com>
2025-04-01 16:43:16 -0700

    update title and color config

--- _config.ts
+++ _config.ts
@@ -10,6 +10,11 @@ site.use(xeo({
     text:
       "https://fonts.google.com/share?selection.family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900",
   },
+  colors: {
+    hue: 139,
+    complement: 318,
+    analogous: 5,
+  },
 }));
 
 export default site;
