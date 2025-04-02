94793523a176f4f3da4056bc12bd4ded241aff18
Ricky de Laveaga <git@rdela.com>
2025-04-01 18:56:36 -0700

    simplify config

--- _config.ts
+++ _config.ts
@@ -11,23 +11,9 @@ site.use(xeo({
       "https://fonts.google.com/share?selection.family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900",
   },
   colors: {
-    // HSL hues
-    // Pass an integer 1-359 for each of the 3 hue values, to get type checking.
     hue: 288,
     complement: 108,
     analogous: 154,
-
-    // color mixing values
-    sathi: 90, // 80-100
-    satmid: 60, // 50-70
-    satlo: 20, // 10-30
-    xlight: 88, // 84-92
-    lighter: 80, // 76-84
-    lightness: 68, // 64-72
-    midrange: 56, // 48-64
-    lowmid: 32, // 28-36
-    darkness: 20, // 16-24
-    darker: 6, // 0-12
   },
 }));
 
